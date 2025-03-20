15391a672fc4421e78f0acb60d2566fc8bd4e705
mariam george <mariam0107@gmail.com>
2025-03-20 14:47:32 +0100

    Added try catch code,completed the app

--- src/com/mariam/weather/WeatherApp.java
+++ src/com/mariam/weather/WeatherApp.java
@@ -1,10 +1,24 @@
 package com.mariam.weather;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WeatherApp {
-    public static void main(String[] args) throws IOException {
-        WeatherAPI api = new WeatherAPI();
-        System.out.println(api.getForecast("Detroit"));
+    public static void main(String[] args)  {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What city would you like to see the weather forecast for?: ");
+            String city = scanner.nextLine();
+
+            WeatherAPI api = new WeatherAPI();
+            String forecastString = api.getForecast(city);
+
+            WeatherResponseParser parser = new WeatherResponseParser();
+            parser.parseAndPrint(forecastString);
+        }
+        catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+
     }
 }
--- src/com/mariam/weather/WeatherResponseParser.java
+++ src/com/mariam/weather/WeatherResponseParser.java
@@ -1,2 +1,31 @@
-package com.mariam.weather;public class WeatherResponseParser {
+package com.mariam.weather;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WeatherResponseParser {
+    public void parseAndPrint(String jsonResponse) {
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+
+        if(jsonObject.getInt("cod") == 200){
+            System.out.println("Weather forcast for city:");
+            JSONArray forecasts = jsonObject.getJSONArray("list");
+            for (int i=0; i< forecasts.length();i++){
+                JSONObject forecast = forecasts.getJSONObject(i);
+                long timestamp = forecast.getLong("dt");
+                String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date(timestamp * 1000));
+                double temperature = forecast.getJSONObject("main").getDouble("temp");
+                String description = forecast.getJSONArray("weather").getJSONObject(0).getString("description");
+                double humidity = forecast.getJSONObject("main").getDouble("humidity");
+
+                System.out.println(date + ": " + temperature +"C, " + humidity + "% , " +description);
+            }
+        } else {
+            System.out.println("Request contains an error !");
+        }
+    }
 }
+
